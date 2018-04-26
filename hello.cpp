dd2013e605c44d8aa64beed6a9c6d012b4089f42
tandracse <tanudast95@gmail.com>
2018-04-26 16:06:13 +0600

    i update the code

---
+++ hello.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int a,b,c;
+    cout<<"1.Current medication.\n2.New medication\n3.Formulary";
+    cout<<"Enter the number : ";
+    cin>>n;
+    for(int i=1; i<=n; i++)
+    {
+        if(i==1)
+        {
+            first();
+        }
+        else if(i==2)
+        {
+            second();
+        }
+        else
+        {
+            third();
+        }
+    }
+
+
+
+}
