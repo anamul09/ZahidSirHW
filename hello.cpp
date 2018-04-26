d74555fee74af30bf6af0d454662c35637d40d99
tandracse <tanudast95@gmail.com>
2018-04-26 15:57:02 +0600

    i write for loop to write the conditions.

--- hello.cpp
+++ hello.cpp
@@ -6,8 +6,14 @@ int main()
     cout<<"";
     cout<<"update github" << endl;
     cout << "update github" <<endl;
+    int a,b,c;
+    cout<<"Enter the number : ";
+    cin>>a;
+    for(int i=1;i<=a;i++)
+    {
 
-    
+    }
+
 
 
 }
