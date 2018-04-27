acc616935daeac33a0e6d31b505b90fb61dad6b2
Bikash Halder <bikashjucse247@gmail.com>
2018-04-27 23:39:37 +0600

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 void  First_display()
 {
-
   cout<<"\t1.If I want to change \n\t2.If I don't want to change.\n";
   cin>>n;
   if(n==1)
@@ -13,29 +12,15 @@ void  First_display()
   }
   else{
    cout<<"Do the same things.";
-
-  cout<<"\t1.If you wish to change \n\t2.no\n";
-  cin>>n;
-  if(n==1)
-  {
-    cout<<"It napa before and after dinner and breakfast.\n";
-  }
-  else{
-   cout<<"continue as before";
-
   }
 }
 void  second_display()
 {
-
   cout<<"\t1.If you wish to modify \n\t2.You don't want to modify.\n";
-
-  cout<<"\t1.If you wish to modify \n\t2.no\n";
-
   cin>>n;
   if(n==1)
   {
-    cout<<"If you sick please contact with doctor.\n";
+    cout<<"If you ill early contact with doctor.\n";
   }
   else{
  cout << "You will suffer in the long run.";
@@ -43,12 +28,12 @@ void  second_display()
 }
 void  third_display()
 {
-  cout<<"\t1.If you wish to change \n\t2.no\n";
+  cout<<"\t1.If you wish to change \n\t2.no change\n";
   cin>>n;
   if(n==1)
   {
-    cout << "Change the formularity\n";
-        cout << "Take new Formular as:\n";
+    cout << "\tChange the formularity\n";
+        cout << "\tTake new Formular as:\n";
         cout << "\t\t1.Sudfed\n";
         cout << "\t\t2.DayQuil\n";
         cout << "\t\t3.NyQuil\n";
@@ -59,11 +44,11 @@ void  third_display()
         }
         else if (n == 2)
         {
-            cout << "DayQuil take 2 times\n";
+            cout << "DayQuil take 2 times in a day.\n";
         }
         else if (n == 3)
         {
-            cout << "NyQuil take 3 times before eating breakfast, dinner and launch.\n";
+            cout << "NyQuil take 3 times before eating breakfast,  after dinner and before launch.\n";
         }
   }
   else{
